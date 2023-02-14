500ca534f2dfc2499a54a9ebae273d03f92e001c
yawarakacream <70136871+yawarakacream@users.noreply.github.com>
2023-02-14 15:22:50 +0900

    Update format

--- src/utility/tree_string_builder.rs
+++ src/utility/tree_string_builder.rs
@@ -12,21 +12,44 @@ impl TreeStringBuilder {
     }
 
     pub fn build(&self) -> String {
+        let mut max_level = 0;
+        for node in &self.nodes {
+            max_level = max_level.max(node.0);
+        }
+
+        let mut levels_to_below = vec![vec![false; max_level + 1]; self.nodes.len()];
+        {
+            let i = self.nodes.len() - 1;
+            levels_to_below[i][self.nodes[i].0] = true;
+        }
+        for i in (1..(self.nodes.len() - 1)).rev() {
+            let node = &self.nodes[i];
+            for l in 1..(node.0) {
+                levels_to_below[i][l] = levels_to_below[i + 1][l];
+            }
+            levels_to_below[i][node.0] = true;
+        }
+
         let mut buffer = String::new();
-        for (level, content) in &self.nodes {
-            Self::write_indent(&mut buffer, *level);
+        for (i, (level, content)) in (&self.nodes).iter().enumerate() {
+            if 0 < i {
+                for l in 1..*level {
+                    if levels_to_below[i][l] {
+                        buffer.push_str("│  ");
+                    } else {
+                        buffer.push_str("   ");
+                    }
+                }
+                if i + 1 < self.nodes.len() && levels_to_below[i + 1][*level] {
+                    buffer.push_str("├─ ");
+                } else {
+                    buffer.push_str("└─ ");
+                }
+            }
+
             buffer.push_str(content);
             buffer.push('\n');
         }
         buffer
-    }
-
-    fn write_indent(buffer: &mut String, level: usize) {
-        if 0 < level {
-            for _ in 0..(level - 1) {
-                buffer.push_str("   ");
-            }
-            buffer.push_str("├─ ");
-        }
     }
 }
